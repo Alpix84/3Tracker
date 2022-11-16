27c9d3a0292aee4ece580beb00588f04bb4b2498
AlpiX84 <alpar0184@gmail.com>
2022-11-16 11:35:57 +0200

    Splash Screen created, basic files added

---
+++ app/src/main/java/com/example/a3tracker/MainFragments/ActivitiesFeed.kt
@@ -0,0 +1,25 @@
+package com.example.a3tracker.MainFragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a3tracker.R
+
+class ActivitiesFeed : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activities_feed, container, false)
+    }
+}
